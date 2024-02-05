e192ed89074675d8c94cacaa1240d4a37d30f23c
DUY TRAN <tdduy.dev@gmail.com>
2024-02-05 16:01:39 +0700

    update thme

---
+++ static/js/youtubePlayer.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var youtube = document.querySelectorAll(".youtube-player");
+
+    for (var i = 0; i < youtube.length; i++) {
+        var source = "https://img.youtube.com/vi/" + youtube[i].dataset.id + "/sddefault.jpg";
+
+        var image = new Image();
+        image.src = source;
+        image.addEventListener("load", function () {
+            youtube[i].appendChild(image);
+        }(i));
+
+        youtube[i].addEventListener("click", function () {
+
+            var iframe = document.createElement("iframe");
+
+            iframe.setAttribute("frameborder", "0");
+            iframe.setAttribute("allowfullscreen", "");
+            iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.id + "?rel=0&showinfo=0&autoplay=1");
+
+            this.innerHTML = "";
+            this.appendChild(iframe);
+        });
+    };
+});
